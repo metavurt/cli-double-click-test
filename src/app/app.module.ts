241b2d8cc876a745d5b66fd9637b3e0397897661
William ONeal <metavurt@gmail.com>
2017-03-12 15:23:30 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+
+const ROUTES = [
+  { path: '', redirectTo: 'work', pathMatch: 'full' },
+  { path: 'work', component: WorkComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(ROUTES)    
   ],
   providers: [],
   bootstrap: [AppComponent]
